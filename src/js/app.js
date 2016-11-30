a8ac3f0cc3efa6b1b11861b5cbd759d4993d2fbf
Eric W. Greene <eric@ericwgreene.com>
2016-11-30 15:03:13 -0800

    added code demos

--- src/js/app.js
+++ src/js/app.js
@@ -3,8 +3,26 @@ import ReactDOM from 'react-dom';
 
 class ColorTool extends React.Component {
 
+	constructor(props) {
+
+		super(props);
+
+		this.state = {
+			newColor: ''
+		};
+
+		this.onChange = this.onChange.bind(this);
+	}
+
+	onChange(e) {
+		this.setState({
+			[e.target.name]: e.target.value
+		});
+	}
+
 	render() {
-		
+
+		console.log('render');
 
 		//console.log(Object.isFrozen(this.props));
 		//this.props.toolHeader = 'New Tool Header';
@@ -17,6 +35,11 @@ class ColorTool extends React.Component {
 			<ul>
 				{this.props.colors.map(color => <li>{color}</li>)}
 			</ul>
+			<form>
+				<label htmlFor="new-color">New Color:</label>
+				<input type="text" id="new-color" name="newColor" onChange={this.onChange} value={this.state.newColor} />
+				<button type="button">Add Color</button>
+			</form>
 		</div>;
 	}
 }
