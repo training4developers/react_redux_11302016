6c3f011bf1eb5720cacb6d6642000f88b57e37ba
Eric W. Greene <eric@ericwgreene.com>
2016-12-01 21:10:37 -0800

    added widgets redux program

---
+++ demos/js/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import 'bootstrap-loader';
+import '../css/styles.scss';
+
+export class WidgetTool extends Component {
+
+	render() {
+		return <div className="">Widget Tool</div>;
+	}
+
+}
+
+ReactDOM.render(<WidgetTool />, document.querySelector('my-app'));
